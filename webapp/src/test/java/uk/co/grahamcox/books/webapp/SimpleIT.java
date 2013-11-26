8cad025de0f98d459d4c9d95e9f8d45b0e564796
Graham Cox <graham.cox@oclc.org>
2013-11-26 17:08:30 +0000

    Added failsafe into the mix for integration tests

---
+++ webapp/src/test/java/uk/co/grahamcox/books/webapp/SimpleIT.java
@@ -0,0 +1,28 @@
+/****************************************************************************************************************
+ *
+ *  Copyright (c) 2013 OCLC, Inc. All Rights Reserved.
+ *
+ *  OCLC proprietary information: the enclosed materials contain
+ *  proprietary information of OCLC, Inc. and shall not be disclosed in whole or in
+ *  any part to any third party or used by any person for any purpose, without written
+ *  consent of OCLC, Inc.  Duplication of any portion of these materials shall include this notice.
+ *
+ ******************************************************************************************************************/
+package uk.co.grahamcox.books.webapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Simple test class to make cobertura happy
+ */
+public class SimpleIT {
+    /**
+     * Simple test
+     */
+    @Test
+    public void test() {
+        Assert.assertEquals(1, 1);
+    }
+
+}
