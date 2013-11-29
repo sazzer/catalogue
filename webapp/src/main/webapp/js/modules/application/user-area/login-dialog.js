70214d2815b6ead0c6e7a728937c64d71537b119
Graham Cox <graham.cox@oclc.org>
2013-11-29 17:14:29 +0000

    Javascript to dynamically create the list of providers in the login dialog

--- webapp/src/main/webapp/js/modules/application/user-area/login-dialog.js
+++ webapp/src/main/webapp/js/modules/application/user-area/login-dialog.js
@@ -4,10 +4,12 @@
 define([
   "dojo/_base/declare",
   "dojo/i18n",
+  "dojo/dom-construct",
+  "dojo/query",
   "dijit/Dialog",
   "dojo/text!./templates/login-dialog.tmpl",
   "dojo/i18n!./nls/login-dialog"
-  ], function(declare, i18n, Dialog, template) {
+  ], function(declare, i18n, domConstruct, query, Dialog, template) {
   return declare([], {
     /**
      * Constructor that will actually build the dialog
@@ -15,8 +17,7 @@ define([
     constructor: function() {
       this.messages = i18n.getLocalization("books.application.user-area", "login-dialog", this.lang);
       this._dialog = new Dialog({
-        title: this.messages.title,
-        content: template
+        title: this.messages.title
       });
     },
 
@@ -26,6 +27,28 @@ define([
      * @public
      */
     show: function() {
+      var dialogContent = domConstruct.toDom(template),
+          providerList = query(".socialButtons", dialogContent),
+          providers = [{
+              id: "facebook",
+              label: "Sign in with Facebook"
+          }, {
+              id: "google",
+              label: "Sign in with Google"
+          }, {
+              id: "twitter",
+              label: "Sign in with Twitter"
+          }, ];
+
+      providers.forEach(function(provider) {
+          var providerNode = domConstruct.create("li", {
+              "class": provider.id,
+              "innerHTML": provider.label
+          });
+          domConstruct.place(providerNode, providerList[0], "last");
+      });
+
+      this._dialog.set("content", dialogContent);
       this._dialog.show();
     }
   });
